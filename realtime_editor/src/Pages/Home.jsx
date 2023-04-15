191f31a0da146be58909c8158b1eb4d66afcb8aa
RahulB <rahulbisht1012@gmail.com>
2023-04-15 17:52:39 +0530

    Unique Id generation Task Completed and Now creating toast

--- realtime_editor/src/Pages/Home.jsx
+++ realtime_editor/src/Pages/Home.jsx
@@ -1,6 +1,22 @@
 import React from 'react'
+import { useState } from 'react'
+import { v4 as uuidV4 } from 'uuid'
+
 
 const Home = () => {
+
+    const [roomId, setRoomId] = useState('')
+    const [userName, setUserName] = useState('')
+
+    const createNewRoom = (e) => {
+        e.preventDefault()
+
+        const id = uuidV4()
+        // console.log(id)
+        setRoomId(id)
+
+    }
+
     return (
         <div className='homePageWrapper'>
             <div className="formWrapper">
@@ -15,12 +31,24 @@ const Home = () => {
                         type="text"
                         className='inputBox'
                         placeholder='Room Id'
+                        onChange={(e) => {
+                            setRoomId(e.target.value)
+                            console.log(e.target.value)
+                        }
+                        }
+                        value={roomId}
                     />
 
                     <input
                         type="text"
                         className='inputBox'
                         placeholder='User Name'
+                        onChange={(e) => {
+                            setUserName(e.target.value)
+                            console.log(e.target.value)
+                        }
+                        }
+                        value={userName}
                     />
 
 
@@ -30,7 +58,11 @@ const Home = () => {
 
                     <span className='createInfo'>
                         If you don't have an invite then create &nbsp;
-                        <a href="/editor" className='createNewBtn'>
+                        <a
+                            href="/editor"
+                            className='createNewBtn'
+                            onClick={createNewRoom}
+                        >
                             New Room
                         </a>
                     </span>
